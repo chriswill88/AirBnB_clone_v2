30de6545535a7738e2a6186aafea3f0f97fe212a
chriswill88 <williechri79@gmail.com>
2020-01-22 17:40:31 -0500

    [ADD] task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from flask import Flask
+"""Modual For Simple Flask"""
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """Function for Flask Web Application"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Function for Flask Web Application"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def cisfun(text):
+    """Function for Flask Web Application"""
+    return 'c {}'.format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port="5000")
