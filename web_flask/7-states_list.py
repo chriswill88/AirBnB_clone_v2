2728460cca46117ecf5ab18f8f0f8d00905ff9cb
chriswill88 <williechri79@gmail.com>
2020-03-01 02:17:28 -0500

    [FIX] sorted states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,15 +12,18 @@ app.url_map.strict_slashes = False
 def state_():
     """Function for Flask Web Application"""
     states = {}
+    s_states = {}
     alls = storage.all('State')
     # print(alls)
     for k, v in alls.items():
         name_of, id = k.split(".")[0], k.split(".")[1]
         name = v.to_dict()['name']
-        # print(name, name_of)
         states[id] = name
-        # print("states ->>>>>", states)
-    return render_template('7-states_list.html', nom="States", states=states)
+
+    for key, value in sorted(states.items(), key=lambda item: item[1]):
+        s_states[key] = value
+    # print("states ->>>>>", states)
+    return render_template('7-states_list.html', nom="States", states=s_states)
 
 
 @app.teardown_appcontext
